4ed7da5e27a1e8f9852b31dd73278e5f8c84eda6
Tim B <tbatty7@gmail.com>
2019-06-08 12:11:04 -0400

    created producer to send String messages to Kafka

--- src/main/java/com/battybuilds/kafkaproducer/Producer.java
+++ src/main/java/com/battybuilds/kafkaproducer/Producer.java
@@ -1,15 +1,30 @@
 package com.battybuilds.kafkaproducer;
 
+import com.battybuilds.kafkaproducer.avro.model.AvroHttpRequest;
+import com.battybuilds.kafkaproducer.avro.model.ClientIdentifier;
+import com.battybuilds.kafkaproducer.config.MyMessageChannels;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
+@EnableBinding(MyMessageChannels.class)
 public class Producer {
+
+    private final MyMessageChannels myMessageChannels;
+
+    public Producer(MyMessageChannels myMessageChannels) {
+        this.myMessageChannels = myMessageChannels;
+    }
 
     @GetMapping("/")
     public String sendMessage() {
-
+        myMessageChannels.outputChannel().send(MessageBuilder.withPayload("hello").build());
 
         return "message NOT sent!";
     }
+    
 }
