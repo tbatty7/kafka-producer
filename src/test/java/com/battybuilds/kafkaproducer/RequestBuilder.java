2d68577af50e77bb88d4dcefed046a293e913822
Tim B <tbatty7@gmail.com>
2019-06-08 11:29:00 -0400

    added utility class to build requests for tests

--- src/test/java/com/battybuilds/kafkaproducer/RequestBuilder.java
+++ src/test/java/com/battybuilds/kafkaproducer/RequestBuilder.java
@@ -1,11 +1,21 @@
 package com.battybuilds.kafkaproducer;
 
 import com.battybuilds.kafkaproducer.avro.model.AvroHttpRequest;
+import com.battybuilds.kafkaproducer.avro.model.ClientIdentifier;
+
+import java.util.Arrays;
 
 public class RequestBuilder {
 
     public static AvroHttpRequest createRequest() {
-        
-        return null;
+        ClientIdentifier clientIdentifier = ClientIdentifier.newBuilder()
+                .setHostName("Hostess Twinkies")
+                .setIpAddress("Ip Man lives in China?")
+                .build();
+        return AvroHttpRequest.newBuilder()
+                .setClientIdentifier(clientIdentifier)
+                .setEmployeeNames(Arrays.asList("Yohan", "Sebastian", "Bach"))
+                .setRequestTime(3L)
+                .build();
     }
 }
